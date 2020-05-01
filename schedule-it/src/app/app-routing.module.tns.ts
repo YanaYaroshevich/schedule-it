5895dddac55789a3c4a1e165afedaaacae8948ee
yyarashevich <yyaroshevich@exadel.com>
2020-05-01 19:34:00 +0300

    Add nativescript image

--- schedule-it/src/app/app-routing.module.tns.ts
+++ schedule-it/src/app/app-routing.module.tns.ts
@@ -2,8 +2,19 @@ import {NgModule} from '@angular/core';
 import {NativeScriptRouterModule} from 'nativescript-angular/router';
 import {routes} from '@src/app/app.routes';
 
+import { TNSImageModule } from 'nativescript-image/angular';
+import * as imageModule from 'nativescript-image';
+import * as applicationModule from '@nativescript/core/application';
+
+if (applicationModule.android) {
+	applicationModule.on(applicationModule.launchEvent, () => {
+		console.log('initialize pipeline');
+		imageModule.initialize();
+	});
+}
+
 @NgModule({
-	imports: [NativeScriptRouterModule.forRoot(routes)],
+	imports: [NativeScriptRouterModule.forRoot(routes), TNSImageModule],
 	exports: [NativeScriptRouterModule]
 })
 export class AppRoutingModule {}
--- schedule-it/src/app/pages/intro/intro.component.ts
+++ schedule-it/src/app/pages/intro/intro.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit, } from "@angular/core";
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 
 @Component({
-    selector: "intro",
-    templateUrl: "./intro.component.html",
-    styleUrls: ["./intro.component.scss"]
+	selector: 'intro',
+	templateUrl: './intro.component.html',
+	styleUrls: ['./intro.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class IntroComponent implements OnInit {
-
-    constructor() { }
+	constructor() {}
 
 	ngOnInit(): void {
 		//this.page.actionBarHidden = true;
